c87d69132fd91410bfb663ec76310a7c6f4f31ec
Romain Loeuillet <loeuillet.romain@gmail.com>
2018-08-16 14:54:52 +0200

    back-end done

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,18 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.create!(params.require(:cocktail).permit(:name))
+    redirect_to cocktail_path(@cocktail)
+  end
+end
